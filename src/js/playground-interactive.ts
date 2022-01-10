fe4674f52f85f66b3bde9911c9f0ebda086fd54a
Luna-Klatzer <luna.klatzer@gmail.com>
2022-01-10 11:18:23 +0100

    Removed `/playground/` folder, as the implementation will likely not utilise iframes, added `<noscript>` tags to all files, cleaned up code and removed `window.onload` for playground.html

--- src/js/playground-interactive.ts
+++ src/js/playground-interactive.ts
@@ -2,79 +2,74 @@
  * File for main.html - Currently useless
  */
 
+// main items
 const codeInput: HTMLTextAreaElement = document.querySelector("#code-editor-textarea");
 const textSavingState: HTMLDivElement = document.querySelector("#text-saving-state");
-const IDRunCode: HTMLDivElement = document.querySelector("#IDRunCode");
-const IDConsoleOutput: HTMLDivElement = document.querySelector("#side-editor");
-const IDCompilerOutput: HTMLDivElement = document.querySelector("#side-editor");
 
-const ConsoleOutputButton: HTMLDivElement = document.querySelector("#console-output-button");
-const CompilerOutputButton: HTMLDivElement = document.querySelector("#compiler-output-button");
+// menu buttons
+const runCodeListItem: HTMLLIElement = document.querySelector("#run-code-list-item");
+let runCodeButton: HTMLButtonElement = document.querySelector("#run-code-list-item button");
+const copyCodeButton: HTMLButtonElement = document.querySelector("#copy-code-list-item button");
+const clearContentButton: HTMLButtonElement = document.querySelector("#clear-content-list-item button");
+
+// for now, don't update the side-editor
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const sideEditor: HTMLDivElement = document.querySelector("#side-editor");
+
+// sidebar buttons
+const consoleOutputButton: HTMLButtonElement = document.querySelector("#console-output-button button");
+const compilerOutputButton: HTMLButtonElement = document.querySelector("#compiler-output-button button");
+
+let ScriptRunning: boolean;
 
 // reset previously entered text
 codeInput.value = localStorage.getItem("code-editor-textarea");
 
-const OutputConsole: HTMLDivElement = IDConsoleOutput;
-const OutputCompiler: HTMLDivElement = IDCompilerOutput;
-
-let CompilerShowed: boolean;
-let ScriptRunning: boolean;
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function runCode() {
-  console.log("Run Code!");
+  console.log("Running Code!");
   ScriptRunning = true;
-  textSavingState.innerHTML = `<p class="gray-text">Running...</p>`;
-  IDRunCode.innerHTML = `<button onclick="stopCode(this)" id="stopCode">Stop</button>`;
-  const currentTime = new Date();
-  const time = (currentTime.getHours() >= 10 ? currentTime.getHours(): ("0" + currentTime.getHours()))+ ":" + (currentTime.getMinutes() >= 10 ? currentTime.getMinutes() : ("0" + currentTime.getHours()));
-  IDCompilerOutput.innerHTML = time +`<p class="gray-text">: Compilation started...<br>\n</p>`;
-  IDConsoleOutput.innerHTML =  time +`<p class="gray-text">: Script started...<br>\n</p>`;
-  console.log("Compiler: " + IDCompilerOutput.innerHTML);
-  console.log("Console: " + IDConsoleOutput.innerHTML);
-  //TODO:Really run the Code
-}
+  runCodeListItem.innerHTML = `<button>Stop</button>`;
+  runCodeButton = document.querySelector("#run-code-list-item button");
+  runCodeButton.addEventListener("click", stopCode);
 
-function stopCode() {
-  console.log("Stopped!");
-  ScriptRunning = false;
-  textSavingState.innerHTML = `<p class="gray-text">Stopped...</p>`;
-  IDRunCode.innerHTML = `<button onclick="runCode(this)" id="runCode">Run</button>`;
-  const currentTime = new Date();
-  const time = (currentTime.getHours() >= 10 ? currentTime.getHours(): ("0" + currentTime.getHours()))+ ":" + (currentTime.getMinutes() >= 10 ? currentTime.getMinutes() : ("0" + currentTime.getHours()));
-  IDConsoleOutput.innerHTML = IDConsoleOutput.innerHTML + time +`<p class="gray-text">: Script stopped...</p>`;
-  console.log("Console: " + IDConsoleOutput.innerHTML);
-
-  //TODO: Really stop the Code
+  // TODO: Actually run the Code
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function clearContents() {
+function stopCode() {
+  console.log("Stopped!");
+  ScriptRunning = false;
+  runCodeListItem.innerHTML = `<button>Run</button>`;
+  runCodeButton = document.querySelector("#run-code-list-item button");
+  runCodeButton.addEventListener("click", runCode);
+
+  // TODO: Actually stop the Code
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function clearContent() {
   console.log("Code Cleared!");
   codeInput.value = "";
   textSavingState.innerHTML = `<p class="gray-text">Code cleared!</p>`;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-function copy() {
+function copyContent() {
   console.log("Code Copied!");
-  navigator.clipboard.writeText(codeInput.value);
-  textSavingState.innerHTML = `<p class="gray-text">Code copied!</p>`;
+  navigator.clipboard.writeText(codeInput.value).then(() => {
+    textSavingState.innerHTML = `<p class="gray-text">Code copied!</p>`;
+  });
 }
 
-function ConsoleOutput() {
-  console.log("Switched to Console Output");
-  ConsoleOutputButton.innerHTML = "<b><u><button onclick=\"ConsoleOutput()\">Console Output</button></u></b>";
-  CompilerOutputButton.innerHTML = "<button onclick=\"CompilerOutput()\">Compiler Output</button>";
-  IDConsoleOutput.innerHTML = `<iframe src="https://orf.at/" style="border:0px #ffffff none;" name="consoleOutputIframe" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>`;
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function SwitchToConsoleOutput() {
+  // this will depend on the future implementation. Stop Development on this
 }
 
-function CompilerOutput() {
-  console.log("Switched to Compiler Output");
-  ConsoleOutputButton.innerHTML = "<button onclick=\"ConsoleOutput()\">Console Output</button>";
-  CompilerOutputButton.innerHTML = "<b><u><button onclick=\"CompilerOutput()\">Compiler Output</button></u></b>";
-  CompilerShowed = true;
-  IDCompilerOutput.innerHTML = `<iframe src="https://orf.at/" style="border:0px #ffffff none;" name="consoleOutputIframe" scrolling="yes" frameborder="0" marginheight="0px" marginwidth="0px" height="400px" width="600px" allowfullscreen></iframe>`;
-  console.log("Compiler: " + OutputCompiler.innerHTML);
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+function SwitchToCompilerOutput() {
+  // this will depend on the future implementation. Stop Development on this
 }
 
 // if the input is not empty, signalise that code was restored
@@ -83,8 +78,20 @@ if (codeInput.value != "")
 else
   textSavingState.innerHTML = `<p class="gray-text">Start typing! We will save your changes while you are typing!</p>`;
 
+// adding event listeners to the menu buttons
+runCodeButton.addEventListener("click", runCode);
+copyCodeButton.addEventListener("click", copyContent);
+clearContentButton.addEventListener("click", clearContent);
+
+// adding event listeners to the sidebar buttons
+consoleOutputButton.addEventListener("click", SwitchToConsoleOutput);
+compilerOutputButton.addEventListener("click", SwitchToCompilerOutput);
+
+// runtime variable for the writing event listener
 let cancel;
 let spinning: boolean;
+
+// adding keyup listener
 codeInput.addEventListener("keyup", event => {
   // if cancel exists / is active -> clear timeout
   if (cancel)
@@ -123,3 +130,4 @@ codeInput.addEventListener("keyup", event => {
     spinning = true;
   }
 })
+
