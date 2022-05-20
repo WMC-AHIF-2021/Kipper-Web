3ac4849b02e537d5551191cde3e22bbde219d085
MaxiLehner <71698615+MaxiLehner@users.noreply.github.com>
2022-05-20 14:07:14 +0200

    search documents, search Match functions

--- src/js/search-bar.ts
+++ src/js/search-bar.ts
@@ -1,1 +1,29 @@
 /* Implement Search-bar search functionality */
+const parseHTMLString = (() => {
+  const parser = new DOMParser();
+  return str => parser.parseFromString(str, "text/html");
+})();
+
+const getSearchStringForDoc = doc => {
+  return [
+    doc.title,
+    doc.body.innerText
+  ].map(str => str.trim())
+    .join(" ");
+};
+
+const stringMatchesQuery = (str, query) => {
+  str = str.toLowerCase();
+  query = query.toLowerCase();
+
+  return query
+    .split(/\W+/)
+    .some(q => str.includes(q))
+};
+
+const htmlStringMatchesQuery = (str, query) => {
+  const htmlDoc = parseHTMLString(str);
+  const htmlSearchString = getSearchStringForDoc(htmlDoc);
+
+  return stringMatchesQuery(htmlSearchString, query);
+};
