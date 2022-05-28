058d7fe42a25e8332ccb5dc23be511a9a6532c63
Luna-Klatzer <luna.klatzer@gmail.com>
2022-05-28 19:47:31 +0200

    Fixed invalid searchbar URLs

--- src/js/search.ts
+++ src/js/search.ts
@@ -4,10 +4,13 @@ export function capitalizeFirstLetter(string: string): string {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
+export function setSearchVisibility(visible: boolean): void {
+  document.getElementById("search-result").style.visibility = visible ? "visible" : "hidden";
+  document.getElementById("search-result-overlay").style.visibility = visible ? "visible" : "hidden";
+  document.getElementById("search-background-overlay").style.visibility = visible ? "visible" : "hidden";
+}
+
 export async function search(): Promise<void> {
-  const overlayDark = <HTMLDivElement>(
-    document.getElementById("search-background-overlay")
-  );
   const overlaySearch = <HTMLDivElement>(
     document.getElementById("search-result-overlay")
   );
@@ -16,52 +19,51 @@ export async function search(): Promise<void> {
     document.getElementById("search-bar-input")
   );
   const searchBar = <HTMLLIElement>document.getElementById("search-bar");
-  overlayDark.style.visibility = "visible";
-  searchResult.style.visibility = "visible";
-  overlaySearch.style.visibility = "visible";
-  searchResult.innerHTML = "Type to search...";
+
+  // Initialise basic interface
+  setSearchVisibility(true);
+
+  // Get the search text
   const searchInput = searchBarInput.value.toLowerCase();
 
-  const pageResults: Array<PageIndex> = (() => {
-    const results: Array<PageIndex> = [];
-    for (const page of searchIndex) {
-      const title = page.pageTitle.toLowerCase();
-      const description = page.pageDescription.toLowerCase();
+  if (searchInput) {
+    const pageResults: Array<PageIndex> = (() => {
+      const results: Array<PageIndex> = [];
+      for (const page of searchIndex) {
+        const title = page.pageTitle.toLowerCase();
+        const description = page.pageDescription.toLowerCase();
 
-      // If the title or the description includes the string that was searched for, add it to the list
-      if (title.includes(searchInput) || description.includes(searchInput)) {
-        results.push(page);
+        // If the title or the description includes the string that was searched for, add it to the list
+        if (title.includes(searchInput) || description.includes(searchInput)) {
+          results.push(page);
+        }
       }
-    }
-    console.log(results);
-    return results;
-  })();
+      console.log(results);
+      return results;
+    })();
 
-  if (pageResults.length > 0) {
-    let resultsHTML = "";
-    for (const result of pageResults) {
-      const link = result.uriPath;
-      const pageTitle = result.pageTitle;
-      const pageDescription = result.pageDescription;
+    if (pageResults.length > 0) {
+      let resultsHTML = "";
+      for (const result of pageResults) {
+        // @ts-ignore
+        const link = isDocsFile ? `..${result.uriPath}` : result.uriPath;
+        const pageDescription = result.pageDescription;
+        const pageTitle = capitalizeFirstLetter(result.pageTitle);
 
-      // Positive Result at Search
-      console.log(
-        "The Search found: " + link + " for your request! (" + searchInput + ")"
-      );
-      resultsHTML +=
-        '<div><p><a  href="' +
-        link +
-        '">' +
-        capitalizeFirstLetter(pageTitle) +
-        "</a></p><small>" +
-        pageDescription +
-        "</small></div>";
+        // Positive Result at Search
+        console.log(
+          "The Search found: " + link + " for your request! (" + searchInput + ")"
+        );
+        resultsHTML +=
+          `<div><p><a href="${link}">${pageTitle}</a></p><small>${pageDescription}</small></div>`;
+      }
+      searchResult.innerHTML = resultsHTML;
+    } else {
+      // Negative Result at Search
+      searchResult.innerHTML = "Sorry, we couldn't find anything for your search!";
     }
-    document.getElementById("search-result").innerHTML = resultsHTML;
   } else {
-    // Negative Result at Search
-    document.getElementById("search-result").innerHTML =
-      "Sorry, we couldn't find anything for your search!";
+    searchResult.innerHTML = "Type to search...";
   }
 
   // Set position of the search result box
@@ -100,10 +102,10 @@ document.addEventListener("click", (e: MouseEvent) => {
       .contains(<Element>e.target) &&
     !document.getElementById("search-bar").contains(<Element>e.target)
   ) {
-    document.getElementById("search-result").style.visibility = "hidden";
-    document.getElementById("search-result-overlay").style.visibility =
-      "hidden";
-    document.getElementById("search-background-overlay").style.visibility =
-      "hidden";
+    setSearchVisibility(false);
   }
+});
+
+document.addEventListener("scroll", () => {
+  setSearchVisibility(false);
 });
